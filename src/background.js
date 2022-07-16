3b9d1299bdc7fbad1905e9de40eb2bc1eb7e59c3
Aaron Bailey <aaronthesnowboarder@live.com>
2022-07-15 22:14:45 -0700

    added random person generator api

--- src/background.js
+++ src/background.js
@@ -13,12 +13,10 @@ protocol.registerSchemesAsPrivileged([
 async function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
     maxHeight:844,
-    maxWidth:390,
+    maxWidth:400,
     minHeight:844,
-    minWidth:390,
+    minWidth:400,
     webPreferences: {
       
       // Use pluginOptions.nodeIntegration, leave this alone
--- src/components/ProfileCard/ProfileCard.vue
+++ src/components/ProfileCard/ProfileCard.vue
@@ -4,25 +4,45 @@ import TagItemVue from '../Tags/TagItem.vue';
     export default{
         data(){
             return{
-                location:'Seattle'
+                profileImg:"",
+                location:''
             }
         },
         components:{
             TagItemVue,
             
+        },
+        methods:{
+           async getUser(){
+                const res=await fetch('https://randomuser.me/api/');
+                const data= await res.json();
+                const userData= data.results[0];
+                console.log(userData)
+                this.location=userData.location.city
+                this.profileImg= userData.picture.large
+            },
+            
+        },
+        watch:{
+
+        },
+        mounted (){
+            this.getUser()
         }
     }
 </script>
 
 <template>
     <div class="swipeButtons">
-        <button class="dislikeBtn swipeBtn"><fa icon="x"/></button>
+        <button class="dislikeBtn swipeBtn" @click="this.getUser"><fa icon="x"/></button>
         <span class="swipBtnSpacer"></span>
-        <Button class="heartBtn swipeBtn"><fa icon="heart"/> </Button>
+        <Button class="heartBtn swipeBtn" @click="this.getUser"><fa icon="heart"/> </Button>
     </div>
     <div class="card profileCard centerMargin">
         <div class="card-body">
-            <div class="profileImgDiv centerMargin"></div>
+            <div class="profileImgDiv centerMargin">
+                <img :src='profileImg' class='profileImg' >
+            </div>
             <div>
                 <p class="bio">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt ipsum, voluptatem nemo veniam fuga, maiores dignissimos provident necessitatibus voluptatibus enim blanditiis dolorum sapiente pariatur amet ab sed inventore doloremque ratione.</p>
                 <h3>My Interest</h3>
@@ -34,7 +54,7 @@ import TagItemVue from '../Tags/TagItem.vue';
                 <div class="col tagCol"><TagItemVue/></div>
                 <div class="col tagCol"><TagItemVue/></div>
             </div>
-            <div>
+            <div class="location">
                 <h3>My Location</h3>
                 {{location}}
             </div>
